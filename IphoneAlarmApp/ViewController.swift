2d7dfb2bcd2e6d986fbb370b3ead10e84cc394e9
davidangulocorcuera <davidangulocorcuera@gmail.com>
2018-11-04 16:10:35 +0100

    Initial Commit

---
+++ IphoneAlarmApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IphoneAlarmApp
+//
+//  Created by Biolizard on 04/11/2018.
+//  Copyright © 2018 Biolizard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IphoneAlarmAppTests/IphoneAlarmAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  IphoneAlarmAppTests.swift
+//  IphoneAlarmAppTests
+//
+//  Created by Biolizard on 04/11/2018.
+//  Copyright © 2018 Biolizard. All rights reserved.
+//
+
+import XCTest
+@testable import IphoneAlarmApp
+
+class IphoneAlarmAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
